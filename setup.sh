673b57b4f30c4d5d5f43cb4b81b5a55253f851fa
matthewf01 <matthewfugel@gmail.com>
2020-04-01 14:10:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ cd /home/pi/Documents
 wget https://github.com/matthewf01/Webex-Teams-Status-Box/blob/master/webexteams.py
 wget https://github.com/matthewf01/Webex-Teams-Status-Box/blob/master/cronadds
 
-crontab -u pi /home/pi/Documents/Webex-Teams-Status-Box/cronadds
+crontab -u pi /home/pi/Documents/cronadds
 
 sudo pip install webexteamssdk
