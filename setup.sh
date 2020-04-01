9ae0361a36776687e4dd840eeeee93dc0ffb30e4
matthewf01 <matthewfugel@gmail.com>
2020-04-01 14:22:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd /home/pi/Documents
 wget https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/cronadds
-wget https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/webexteams.py
+wget https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/webexteams.py >> webexteams.py
 
 crontab -u pi /home/pi/Documents/cronadds
 
