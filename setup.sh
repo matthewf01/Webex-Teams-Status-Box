e8d017c9ccc335e6d5024dffb23b74a3bc94a8a7
matthewf01 <matthewfugel@gmail.com>
2020-04-01 21:35:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,26 @@
+#!/bin/sh
+#2020-04-01 Matthew Fugel 
+
 cd /home/pi/Documents
-wget https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/cronadds
-wget -O webexteams.py https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/webexteams.py
 
-crontab -u pi /home/pi/Documents/cronadds
+#retrieve user-specific values that will be written to mycredentials.sh file for storing as env variables
+read -p "Enter your WebexTeams admin token: " accessToken
+read -p "Enter the personId of the WebexTeams user you're checking status of: " person
 
-sudo pip install webexteamssdk
+#write the values out to file
+echo "export WEBEX_TEAMS_ACCESS_TOKEN="$accessToken >> mycredentials.sh
+echo "export PERSON="$person >> mycredentials.sh
+
+#import as environment variables now
+source mycredentials.sh
+
+#download script files
+#wget https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/cronadds
+#wget -O webexteams.py https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/webexteams.py
+
+#set up the cronjob schedule using downloaded file
+#crontab -u pi /home/pi/Documents/cronadds
+
+#install dependencies
+#sudo pip install webexteamssdk
+
