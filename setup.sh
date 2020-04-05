217d04e2aa465b40296ec2f01f942f618dced953
matthewf01 <matthewfugel@gmail.com>
2020-04-05 12:41:47 -0400

    Update setup.sh
    comments and cleanup of stuff Im not using anymore

--- setup.sh
+++ setup.sh
@@ -15,23 +15,20 @@ echo " ---saving here for your reference. This file is not used by the script. C
 echo "Environment=WEBEX_TEAMS_ACCESS_TOKEN="$accessToken >> mycredentials.txt
 echo "Environment=PERSON="$person >> mycredentials.txt
 
-#import as environment variables now
-source /home/pi/Documents/mycredentials.sh
-
 #download script files
 wget -O webexteams.py https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/webexteams.py
 wget https://raw.githubusercontent.com/matthewf01/Webex-Teams-Status-Box/master/webexteams.service
 
-#update service file with creds
+#update service file with creds using 'sed' to find&replace 'foo' and 'bar' placeholders with user's credentials.
 sed -i "s/foo/$accessToken/" webexteams.service
 sed -i "s/bar/$person/" webexteams.service
 
-#register app as a service
+#copy the service's unit file out to systemd, then register app as a service
 sudo mv webexteams.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable webexteams.service
 
-#install dependencies
+#install external dependencies
 sudo pip install webexteamssdk
 
 #finally, reboot
